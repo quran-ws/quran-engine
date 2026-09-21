479bdd838f7fc241f5aba761013f0a4ba2e7d57d
Abdullah Ibeid <imobde3@gmail.com>
2026-09-21 11:26:54 +0300

    release: pin the prepared 0.3.0 XCFramework

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .binaryTarget(
             name: "QvpEngine",
             url: "https://github.com/quran-ws/quran-engine/releases/download/v0.3.0/QvpEngine.xcframework.zip",
-            checksum: "8ce6d7b4ccf1c8dad896b2a82a9159b2041965c1e296ad2d0d3f5f11c8b4436a"
+            checksum: "2ebc6424a04c92b11c614b6e14b0862bb65046568174f21e80fbd7dc49c3e5c1"
         ),
         .target(
             name: "QvpKit",
